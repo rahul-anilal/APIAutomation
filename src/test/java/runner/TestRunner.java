8b7bf2621881df3983743cc3ae4fc323db2c9617
rahul-anilal <rahul.samuelanilal@walmart.com>
2025-02-04 15:06:33 +0530

    Updated TestRunner and updated dependencies

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,4 +1,15 @@
 package runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "/Users/r0s0q3t/Desktop/CucumberTest/APIAutomation/src/test/java/features"
+        ,glue={"stepdefinitions"}
+)
+
 public class TestRunner {
+
 }
